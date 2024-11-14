8238990b91609b8896b78327a4cab1e227cfdc5f
Celso Jordão <maildojordao@gmail.com>
2024-11-14 22:20:49 +0000

    Initial commit

---
+++ Quizec/app/src/main/java/pt/isec/a21130067/quizec/Quizec.kt
@@ -0,0 +1,12 @@
+package pt.isec.a21130067.quizec
+
+import android.app.Activity
+import android.os.Bundle
+
+class Quizec : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.quizec)
+    }
+}
+
