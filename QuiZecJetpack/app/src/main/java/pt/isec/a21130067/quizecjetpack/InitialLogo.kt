a2cb3991f501f3f6206169d8ec7c1c4b28fb5f1b
Celso Jordão <maildojordao@gmail.com>
2024-11-16 17:33:51 +0000

    FadeOut inicial logo - Done

--- QuiZecJetpack/app/src/main/java/pt/isec/a21130067/quizecjetpack/InitialLogo.kt
+++ QuiZecJetpack/app/src/main/java/pt/isec/a21130067/quizecjetpack/InitialLogo.kt
@@ -6,9 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -17,9 +23,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavHostController
@@ -61,32 +71,63 @@ fun InitialAnimation(navController: NavHostController) {
 
     Box(
         modifier = Modifier
-            .fillMaxSize()
             .clickable {
                 startFadeOut = true
             }
 
     ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFADD8E6))
+                .padding(30.dp)
+        ) {
+            Text(
+                text = "Welcome to QuiZec",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally),
+                fontSize = 34.sp,
+                lineHeight = 30.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.Black
+            )
 
-        Image(
-            painter = painterResource(id = R.drawable.quizec_logo_no_background),
-            contentDescription = "Logo",
-            modifier = Modifier.graphicsLayer(alpha = alpha)
-                .align(Alignment.Center)
-        )
+            Spacer(modifier = Modifier.height(50.dp))
 
-        if (startFadeOut && alpha == 0f) {
-            LaunchedEffect(Unit) {
-                navController.navigate("initial_screen")
+            Image(
+                painter = painterResource(id = R.drawable.quizec_logo_no_background),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .graphicsLayer(alpha = alpha)
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(150.dp))
+
+            Text(
+                text = "your quiZ partner!",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally),
+                fontSize = 34.sp,
+                lineHeight = 30.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.Black
+            )
+
+            if (startFadeOut && alpha == 0f) {
+                LaunchedEffect(Unit) {
+                    navController.navigate("initial_screen")
+                }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun InitialLogoPreview() {
-    QuiZecJetpackTheme {
-        InitialLogo()
+
+    @Preview(showBackground = true)
+    @Composable
+    fun InitialLogoPreview() {
+        QuiZecJetpackTheme {
+            InitialLogo()
+        }
     }
-}
